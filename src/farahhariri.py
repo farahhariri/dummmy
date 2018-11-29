29bd11aecd3e187e23eaa2007aaa72f5c9486622
Farah Hariri <farahhariri@granular.ag>
2018-11-29 12:49:40 -0600

    change

--- src/farahhariri.py
+++ src/farahhariri.py
@@ -8,10 +8,10 @@ def main():
     print("This statement should always be covered")
     res = foo(9)
 
-    # if res > 3:
-    #     res = foo(res)
-    # else:
-    #     res = foo(res + 8)
+    if res > 3:
+        res = foo(res)
+    else:
+        res = foo(res + 8)
     print("THis should also be always printed: " + str(res))
     print(os.path.join("source", "/dest/ina/tion"))
     if res > 3:      
