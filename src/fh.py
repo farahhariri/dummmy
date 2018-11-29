a12d75885dada593f486ed1dfa0e71c601144e27
Farah Hariri <farahhariri@granular.ag>
2018-11-29 12:44:09 -0600

    added a file

---
+++ src/fh.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+import os
+
+def foo(x):
+    return x +5
+
+def main():
+    print("This statement should always be covered")
+    res = foo(9)
+
+    # if res > 3:
+    #     res = foo(res)
+    # else:
+    #     res = foo(res + 8)
+    print("THis should also be always printed: " + str(res))
+    print(os.path.join("source", "/dest/ina/tion"))
+    if res > 3:      
+        res = foo(res)
+    else:
+        res = foo(res + 8)
+
+if __name__ == "__main__":
+    main()
